1a89fd28a4063a8bea7d938311686e87d251b5b9
Eduardo <eduardoordenes421@gmail.com>
2022-10-03 23:15:26 -0300

    "V2.0.4 - Added methods move activity"

--- app/src/main/java/com/programacionandroid/praticaclase2/activity_shipping_address_view.java
+++ app/src/main/java/com/programacionandroid/praticaclase2/activity_shipping_address_view.java
@@ -2,7 +2,9 @@ package com.programacionandroid.praticaclase2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_shipping_address_view extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class activity_shipping_address_view extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shipping_address_view);
     }
+
+    public void moverDeActivityAddress(View view){
+
+        Intent objMover = new Intent(this,MainActivity.class);
+        startActivity(objMover);
+
+    }
+
 }
