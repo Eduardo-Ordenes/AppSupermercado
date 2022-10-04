9190ed9a0ccb1055a5c1d51fa98f63dfeba31ae4
Eduardo <eduardoordenes421@gmail.com>
2022-10-03 23:10:05 -0300

    "V2.0.3 - Added methods move activity"

--- app/src/main/java/com/programacionandroid/praticaclase2/activity_registro_view.java
+++ app/src/main/java/com/programacionandroid/praticaclase2/activity_registro_view.java
@@ -2,7 +2,9 @@ package com.programacionandroid.praticaclase2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_registro_view extends AppCompatActivity {
 
@@ -11,4 +13,21 @@ public class activity_registro_view extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_view);
     }
+
+    public void moverDeActivityRegistro(View view){
+
+        Intent objMover = new Intent(this,activity_login_view.class);
+        startActivity(objMover);
+
+    }
+
+
+    public void moverAPrincipal(View view){
+
+        Intent objMover = new Intent(this,MainActivity.class);
+        startActivity(objMover);
+
+    }
+
+
 }
--- app/src/main/java/com/programacionandroid/praticaclase2/activity_users_view.java
+++ app/src/main/java/com/programacionandroid/praticaclase2/activity_users_view.java
@@ -2,7 +2,9 @@ package com.programacionandroid.praticaclase2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_users_view extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class activity_users_view extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_view);
     }
+
+    public void moverDeActivityUserMedioPago(View view){
+
+        Intent objMover = new Intent(this,activity_cart_view.class);
+        startActivity(objMover);
+
+    }
+
+
 }
